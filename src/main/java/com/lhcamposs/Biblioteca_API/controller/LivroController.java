1a601398703ca72ae9a5223c1b9f5d806dce7a43
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-03-18 10:04:35 -0300

    feat: buscar por id endpoint

--- src/main/java/com/lhcamposs/Biblioteca_API/controller/LivroController.java
+++ src/main/java/com/lhcamposs/Biblioteca_API/controller/LivroController.java
@@ -5,10 +5,7 @@ import com.lhcamposs.Biblioteca_API.service.LivroService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +18,11 @@ public class LivroController {
     public ResponseEntity<Livro> create(@RequestBody Livro livro) {
         Livro book = livroService.salvar(livro);
         return  ResponseEntity.status(HttpStatus.CREATED).body(book);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> getById(@PathVariable Long id){
+        Livro book = livroService.buscarPorId(id);
+        return  ResponseEntity.ok(book);
     }
 }
--- src/main/java/com/lhcamposs/Biblioteca_API/service/LivroService.java
+++ src/main/java/com/lhcamposs/Biblioteca_API/service/LivroService.java
@@ -3,8 +3,10 @@ package com.lhcamposs.Biblioteca_API.service;
 import com.lhcamposs.Biblioteca_API.entity.Livro;
 import com.lhcamposs.Biblioteca_API.repository.LivroRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Service
@@ -16,5 +18,12 @@ public class LivroService {
     public Livro salvar(Livro livro){
 
         return livroRepository.save(livro);
+    }
+
+
+    public Livro buscarPorId(Long id) {
+        return livroRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Livro não encontrado")
+        );
     }
 }
